0a8dd9150f963959697f740d3017ccd90b0497d1
Roy Konzizky <roeicop110@gmail.com>
2023-07-06 11:07:57 +0300

    -fixed the label. -added the functions to the pre-game parts of the vs-cpu mode.

--- src/Menus/CPU/CpuMenu.tsx
+++ src/Menus/CPU/CpuMenu.tsx
@@ -1,46 +1,68 @@
-import './CpuMenu.css';
+import "./CpuMenu.css";
 import { Link } from "react-router-dom";
-import {player1, player2} from "../../Player.tsx";
-
+import { player1, player2 } from "../../Player.tsx";
 
 function CpuMenu() {
-    let badInputText="";
-    const badInputLabel = document.getElementById("badInputLabel") as HTMLElement;
-    const buttonLink : HTMLButtonElement = document.getElementById("buttonLink") as HTMLButtonElement;
-    function submitNames() {
-        player1.name = (document.getElementById("player1Name") as HTMLInputElement).value;
-        player1.sign = (document.getElementById("player1Sign") as HTMLInputElement).value;
-        player1.score = 0;
+  let badInputText = "";
 
-        if (player1.sign===""){
-            player1.sign = "X";
-        }
-
-        if (player1.sign.length>1){
-            badInputText = "signs can only be a single character";
-            badInputLabel.innerText = badInputText;
-            buttonLink.disabled = true;
-        }
-        player2.name="skynet";
-        player2.sign="O";
-        player2.score=0;
+  function submitPlayers() {
+    player1.name = (
+      document.getElementById("player1Name") as HTMLInputElement
+    ).value;
+    player1.sign = (
+      document.getElementById("player1Sign") as HTMLInputElement
+    ).value;
+    player1.score = 0;
+    if (player1.sign === "") {
+      player1.sign = "X";
     }
 
-    return (
-        <>
-            <br></br>
-            <input id="player1Name" placeholder={"player 1's name"}/>
-            <br></br>
-            <input id="player1Sign" placeholder={"player 1's sign"} />
-            <br></br>
-            <Link to="/gamecpu">
-                <button className="buttonLink" type="button" onClick={submitNames}>
-                    Submit Player Sign and Name
-                </button>
-            </Link>
-            <label id={"badInputLabel"}></label>
-        </>
-    );
+    player2.name = "skynet";
+    player2.sign = "O";
+    player2.score = 0;
+
+    checkInput();
+  }
+
+  function checkInput() {
+    const badInputLabel = document.getElementById(
+      "badInputLabel",
+    ) as HTMLElement;
+    const link = document.getElementById("link") as HTMLElement;
+    if (player1.sign.length > 1) {
+      badInputText = "signs can only be a single character";
+      badInputLabel.textContent = badInputText;
+      return;
+    }
+    if (player1.name.length === 0) {
+      badInputText = "players names not entered";
+      badInputLabel.textContent = badInputText;
+      return;
+    } else {
+      link.style.pointerEvents = "auto";
+    }
+  }
+
+  return (
+    <>
+      <br></br>
+      <input id="player1Name" placeholder={"player 1's name"} />
+      <br></br>
+      <input id="player1Sign" placeholder={"player 1's sign"} />
+      <br></br>
+      <button className="buttonLink" type="button" onClick={submitPlayers}>
+        <Link id={"link"} to="/gamepvp">
+          Submit Player Sign and Name
+        </Link>
+      </button>
+
+      <label id={"badInputLabel"}></label>
+    </>
+  );
 }
 
-export default CpuMenu;player1;
+export default CpuMenu;
+player1;
+player2;
+
+//TODO figure out why the label doesnt appear when needed
--- src/Menus/PVP/PvpMenu.tsx
+++ src/Menus/PVP/PvpMenu.tsx
@@ -1,69 +1,79 @@
-import './PvpMenu.css';
+import "./PvpMenu.css";
 import { Link } from "react-router-dom";
-import {player1, player2} from "../../Player.tsx";
-
+import { player1, player2 } from "../../Player.tsx";
 
 function PvpMenu() {
-    let badInputText="";
+  let badInputText = "";
 
-    function submitPlayers() {
-        player1.name = (document.getElementById("player1Name") as HTMLInputElement).value;
-        player2.name = (document.getElementById("player2Name") as HTMLInputElement).value;
-        player1.sign = (document.getElementById("player1Sign") as HTMLInputElement).value;
-        player2.sign = (document.getElementById("player2Sign") as HTMLInputElement).value;
-        player1.score = 0;
-        player2.score = 0;
+  function submitPlayers() {
+    player1.name = (
+      document.getElementById("player1Name") as HTMLInputElement
+    ).value;
+    player2.name = (
+      document.getElementById("player2Name") as HTMLInputElement
+    ).value;
+    player1.sign = (
+      document.getElementById("player1Sign") as HTMLInputElement
+    ).value;
+    player2.sign = (
+      document.getElementById("player2Sign") as HTMLInputElement
+    ).value;
+    player1.score = 0;
+    player2.score = 0;
 
-        if (player1.sign===""){
-            player1.sign = "X";
-        }
-        else if (player2.sign===""){
-            player2.sign = "O";
-        }
-        checkInput();
+    if (player1.sign === "") {
+      player1.sign = "X";
+    } else if (player2.sign === "") {
+      player2.sign = "O";
     }
+    checkInput();
+  }
 
-    function checkInput() {
-        const badInputLabel = document.getElementById("badInputLabel") as HTMLElement;
-        const link = document.getElementById("link") as HTMLElement;
-        if (player1.sign.length>1||player2.sign.length>1){
+  function checkInput() {
+    const badInputLabel = document.getElementById(
+      "badInputLabel",
+    ) as HTMLElement;
+    const link = document.getElementById("link") as HTMLElement;
 
-            badInputText = "signs can only be a single character";
-            badInputLabel.textContent = badInputText;
-            badInputLabel.style.display = "auto";
-            return;
-        }
-        if(player1.name.length===0 || player2.name.length===0){
-            badInputText = "players names not entered";
-            badInputLabel.textContent = badInputText;
-            badInputLabel.style.display = "auto";
-            return;
-        }
-        else{
-            link.style.pointerEvents = "auto";
-        }
+    if (player1.sign.length > 1 || player2.sign.length > 1) {
+      badInputText = "signs can only be a single character";
+      badInputLabel.textContent = badInputText;
+      badInputLabel.style.display = "auto";
+      return;
     }
+    if (player1.name.length === 0 || player2.name.length === 0) {
+      badInputText = "players names not entered";
+      badInputLabel.textContent = badInputText;
+      return;
+    } else {
+      link.style.pointerEvents = "auto";
+    }
+  }
 
-    return (
-        <>
-            <br></br>
-            <input id="player1Name" placeholder={"player 1's name"}/>
-            <br></br>
-            <input id="player1Sign" placeholder={"player 1's sign"} />
-            <br></br>
-            <input id="player2Name" placeholder={"player 2's name"} />
-            <br></br>
-            <input id="player2Sign" placeholder={"player 2's sign"} />
-            <br></br>
-            <button className="buttonLink" type="button" onClick={submitPlayers}>
-                <Link id={"link"} to="/gamepvp">
-                    Submit Player Sign and Name
-                </Link>
-            </button>
-
-            <label id={"badInputLabel"}></label>
-        </>
-    );
+  return (
+    <>
+      <br></br>
+      <input id="player1Name" placeholder={"player 1's name"} />
+      <br></br>
+      <input id="player1Sign" placeholder={"player 1's sign"} />
+      <br></br>
+      <input id="player2Name" placeholder={"player 2's name"} />
+      <br></br>
+      <input id="player2Sign" placeholder={"player 2's sign"} />
+      <br></br>
+      <button className="buttonLink" type="button" onClick={submitPlayers}>
+        <Link id={"link"} to="/gamepvp">
+          Submit Player Sign and Name
+        </Link>
+      </button>
+      <br></br>
+      <label id={"badInputLabel"}></label>
+    </>
+  );
 }
 
-export default PvpMenu;player1;player2;
+export default PvpMenu;
+player1;
+player2;
+
+//TODO make label appear if the input is flawed
