1e5867fab6067dc94f10f5814d4ae80a451cf081
Roy Konzizky <roeicop110@gmail.com>
2023-07-17 23:36:33 +0300

    -latest ver of minmax

--- src/Menus/PVP/PvpMenu.tsx
+++ src/Menus/PVP/PvpMenu.tsx
@@ -1,85 +1,79 @@
 import "./PvpMenu.css";
 import {player1, player2} from "../../PlayableCharacters/Player.ts";
 import {Link} from "react-router-dom";
-import {useState} from "react";
 
 function PvpMenu() {
     let badInputText = "";
-    const [toValue, setToValue] = useState('');
+
     function submitPlayers() {
-        player1.name = (document.getElementById("player1Name") as HTMLInputElement).value;
-        player2.name = (document.getElementById("player2Name") as HTMLInputElement).value;
-
-        player1.sign = (document.getElementById("player1Sign") as HTMLInputElement).value;
-        player2.sign = (document.getElementById("player2Sign") as HTMLInputElement).value;
-
+        player1.name = (
+            document.getElementById("player1Name") as HTMLInputElement
+        ).value;
+        player2.name = (
+            document.getElementById("player2Name") as HTMLInputElement
+        ).value;
+        player1.sign = (
+            document.getElementById("player1Sign") as HTMLInputElement
+        ).value;
+        player2.sign = (
+            document.getElementById("player2Sign") as HTMLInputElement
+        ).value;
         player1.score = 0;
         player2.score = 0;
 
         if (player1.sign === "") {
-            player1.sign = "X.svg.png";
+            player1.sign = "X";
         }
         if (player2.sign === "") {
-            player2.sign = "o.png";
+            player2.sign = "O";
         }
         checkInput();
     }
 
     function checkInput() {
-        const badInputLabel = document.getElementById("badInputLabel") as HTMLElement;
+        const badInputLabel = document.getElementById(
+            "badInputLabel",
+        ) as HTMLElement;
+        const link = document.getElementById("link") as HTMLElement;
 
-        // if (imageExists(player1.sign) || imageExists(player2.sign)) {
-        //     badInputText = "image not found, enter a different path";
-        //     badInputLabel.textContent = badInputText;
-        //     return;
-        // }
-
-        if (player1.name.length === 0 || player2.name.length === 0 || player1.sign === "" || player2.sign === ""){
-            if (player1.sign === "" || player2.sign === "") {
-                badInputText = "image not found, enter a different path";
-                badInputLabel.textContent = badInputText;
-                setToValue('');
-            }
-            if (player1.name.length === 0 || player2.name.length === 0) {
-                badInputText = "players names not entered";
-                badInputLabel.textContent = badInputText;
-                setToValue('');
-            }
-        }else {
-            setToValue('/gamepvp');
+        if (player1.sign.length > 1 || player2.sign.length > 1) {
+            badInputText = "signs can only be a single character";
+            badInputLabel.textContent = badInputText;
+            return;
         }
-
+        if (player1.name.length === 0 || player2.name.length === 0) {
+            badInputText = "players names not entered";
+            badInputLabel.textContent = badInputText;
+            return;
+        } else {
+            link.style.pointerEvents = "auto";
+        }
     }
-
-    // function imageExists(imagePath):boolean{
-    //     if(){
-    //         return true;
-    //     }
-    //     return false
-    // }
 
     return (
         <>
             <div>
-                <input id="player1Name" placeholder={"player 1's name"} onChange={submitPlayers}/>
+                <input id="player1Name" placeholder={"player 1's name"}/>
             </div>
 
             <div>
-                <input id="player1Sign" placeholder={"player 1's sign"} onChange={submitPlayers}/>
+                <input id="player1Sign" placeholder={"player 1's sign"}/>
             </div>
 
             <div>
-                <input id="player2Name" placeholder={"player 2's name"} onChange={submitPlayers}/>
+                <input id="player2Name" placeholder={"player 2's name"}/>
             </div>
 
             <div>
-                <input id="player2Sign" placeholder={"player 2's sign"} onChange={submitPlayers}/>
+                <input id="player2Sign" placeholder={"player 2's sign"}/>
             </div>
 
             <div>
-                <Link className="link" to={toValue}>
-                    Submit Player Sign and Name
-                </Link>
+                <button className="buttonLink" type="button" onClick={submitPlayers}>
+                    <Link id={"link"} to="/gamepvp">
+                        Submit Player Sign and Name
+                    </Link>
+                </button>
             </div>
 
             <div>
@@ -93,5 +87,4 @@ export default PvpMenu;
 player1;
 player2;
 
-//TODO fix the link button, find a way to change it's 'to' attribute
-//ask roy why it doesnt work if i refer to it with id
+//TODO make label appear if the input is flawed
